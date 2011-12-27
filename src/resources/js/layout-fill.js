69c5d8b21f4d312649612fd93829ce9092d5411a
Philip Graham <philip.robert.graham@gmail.com>
2011-12-27 21:18:11 +0000

    Updated fill layout to get reapplied whenever the window is resized

--- src/resources/js/layout-fill.js
+++ src/resources/js/layout-fill.js
@@ -1,22 +1,24 @@
 (function ($, CDT, undefined) {
   "use strict";
 
+  var layouts = [];
+
   if (CDT.layout === undefined) {
     CDT.layout = {};
   }
 
   CDT.layout.fill = function (container) {
-    var layout = {};
+    var layout = {}, sel, apply;
 
-    layout.with = function (selector) {
-      var toFill = container.children(selector),
+    apply = function () {
+      var toFill = container.children(sel),
           height = container.height(),
           allocated = 0,
           remaining,
           fill;
 
       container.children().each(function () {
-        if (!$(this).is(selector)) {
+        if (!$(this).is(sel)) {
           allocated += $(this).outerHeight(true);
         }
       });
@@ -24,8 +26,27 @@
       remaining = height - allocated;
       fill = remaining / toFill.length;
       toFill.height(fill);
+    };
+
+    // Push the apply function onto the list of actions to perform when the
+    // window is resized
+    layouts.push(apply);
+
+    layout.with = function (selector) {
+      sel = selector;
+      apply();
     }
 
     return layout;
   };
+
+  // Add a resize handler to the window which applies all layout functions
+  // TODO Move this into a generic location so that other layout types can
+  //      make use of this functionality
+  $(window).resize(function () {
+    $.each(layouts, function (idx, fn) {
+      fn();
+    });
+  });
+
 } (jQuery, CDT));
