ebc78cbd1de351e5f415e57020a7dd24cc5b759d
Philip Graham <philip.robert.graham@gmail.com>
2012-01-04 22:21:27 +0000

    Updated login javascript and css so that it is only applied if a login form is being displayed.

--- src/resources/js/login.js
+++ src/resources/js/login.js
@@ -1,8 +1,11 @@
 (function ($, undefined) {
 
   $(document).ready(function () {
-    $('#login')
+    var login = $('#login')
       .wrap('<div id="loginWrap"><div id="loginContainer"></div></div>');
+    if (login.length > 0) {
+      $('html,body').addClass('login');
+    }
   });
 
 }) (jQuery);
