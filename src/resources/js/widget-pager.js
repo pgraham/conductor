c73988d19321f7e4987f1dc5898dfdbe8d861099
Philip Graham <philip.robert.graham@gmail.com>
2012-01-29 18:33:58 +0000

    Added a 'Page [ # ] of [ Total ]' label to the pager and refactored the way pager objects are created internally

--- src/resources/js/widget-pager.js
+++ src/resources/js/widget-pager.js
@@ -1,17 +1,72 @@
 (function ($, CDT, undefined) {
   "use strict";
 
-  var DEFAULT_PAGE_SIZE  = 10;
+  var DEFAULT_PAGE_SIZE  = 10,
+      //lblTmpl = stringTemplate('Displaying {0} - {1} of {2}'),
+      pagerize;
 
   if (CDT.widget === undefined) {
     CDT.widget = {};
   }
 
+  pagerize = function (pager) {
+    eventuality(pager);
+
+    pager.getNumPages = function () {
+      return Math.ceil(this.total / this.pageSize);
+    };
+
+    pager.getPaging = function () {
+      return {
+        limit: this.pageSize,
+        offset: (this.curPage - 1) * this.pageSize
+      };
+    };
+
+    pager.setTotal = function (t) {
+      this.total = t;
+      if (this.curPage > this.getNumPages()) {
+        this.curPage = this.getNumPages();
+        this.fire('page-change');
+      }
+      this.update();
+    };
+
+    pager.update = function () {
+      var firstIdx, lastIdx, numPages;
+
+      numPages = this.getNumPages();
+
+      if (this.curPage <= 1) {
+        this.prev.button('option', 'disabled', true);
+        this.first.button('option', 'disabled', true);
+      } else {
+        this.prev.button('option', 'disabled', false);
+        this.first.button('option', 'disabled', false);
+      }
+
+      if (this.curPage >= numPages) {
+        this.next.button('option', 'disabled', true);
+        this.last.button('option', 'disabled', true);
+      } else {
+        this.next.button('option', 'disabled', false);
+        this.last.button('option', 'disabled', false);
+      }
+
+      this.pageNum.val(this.curPage);
+      this.numPages.text('of ' + numPages);
+
+      firstIdx = Math.min(((this.curPage - 1) * this.pageSize) + 1, this.total);
+      lastIdx = Math.min(firstIdx + this.pageSize - 1, this.total);
+
+      //this.lbl.text(lblTmpl.format([firstIdx, lastIdx, this.total]);
+      this.lbl.text('Displaying ' + firstIdx + ' - ' + lastIdx + ' of ' + this.total);
+    };
+  };
+
   CDT.widget.pager = function (pageSize) {
     var pager, elm, first, prev, next, last, pageNum, lbl, update,
         curPage = 1, total, numPages;
-
-    pageSize = pageSize || DEFAULT_PAGE_SIZE;
 
     first = $('<button/>').text('<<').button({
       icons: { primary: 'ui-icon-arrowthickstop-1-w' },
@@ -29,10 +84,12 @@
       icons: { primary: 'ui-icon-arrowthickstop-1-e' },
       text: false
     });
+
+    pageNum = $('<input type="text" class="current-page ui-corner-all" />');
+    numPages = $('<span class="num-pages" />');
+
     lbl = $('<span/>')
       .css('float', 'right');
-
-    pageNum = $('<input type="text"/>');
 
     elm = $('<div class="cdt-pager"/>')
       .addClass('ui-widget-content')
@@ -40,85 +97,57 @@
       .append(prev)
       .append($('<span/>').addClass('page-lbl').text('Page:'))
       .append(pageNum)
+      .append(numPages)
       .append(next)
       .append(last)
       .append(lbl);
 
-    pager = { elm: elm };
-    eventuality(pager);
+    pager = { 
+      elm: elm,
+      first: first,
+      prev: prev,
+      next: next,
+      last: last,
+      pageNum: pageNum,
+      numPages: numPages,
+      lbl: lbl,
+      pageSize: pageSize || DEFAULT_PAGE_SIZE,
+      curPage: 1,
+    };
+    pagerize(pager);
 
     first.click(function () {
-      curPage = 1;
-      update();
+      pager.curPage = 1;
+      pager.update();
       pager.fire('page-change');
     });
     prev.click(function () {
-      curPage -= 1;
-      update();
+      pager.curPage -= 1;
+      pager.update();
       pager.fire('page-change');
     });
     next.click(function () {
-      curPage += 1;
-      update();
+      pager.curPage += 1;
+      pager.update();
       pager.fire('page-change');
     });
     last.click(function () {
-      curPage = Math.ceil(total / pageSize);
-      update();
+      pager.curPage = pager.getNumPages();
+      pager.update();
       pager.fire('page-change');
     });
-
-    update = function () {
-      var firstIdx, lastIdx;
-
-      if (curPage <= 1) {
-        prev.button('option', 'disabled', true);
-        first.button('option', 'disabled', true);
-      } else {
-        prev.button('option', 'disabled', false);
-        first.button('option', 'disabled', false);
-      }
-
-      if (curPage >= numPages) {
-        next.button('option', 'disabled', true);
-        last.button('option', 'disabled', true);
-      } else {
-        next.button('option', 'disabled', false);
-        last.button('option', 'disabled', false);
-      }
-
-      pageNum.val(curPage);
-
-      firstIdx = Math.min(((curPage - 1) * pageSize) + 1, total);
-      lastIdx = Math.min(firstIdx + pageSize - 1, total);
-
-      lbl.text('Displaying ' + firstIdx + ' - ' + lastIdx + ' of ' + total);
-    };
 
     pageNum.on('change', function () {
       var changeTo = parseInt(pageNum.val(), 10);
 
-      if (changeTo > numPages) {
-        pageNum.val(curPage);
+      if (changeTo > pager.getNumPages()) {
+        pageNum.val(pager.curPage);
         return;
       }
 
-      curPage = changeTo;
+      pager.curPage = changeTo;
       pager.fire('page-change');
     });
-
-    pager.getPaging = function () {
-      return {
-        limit: pageSize,
-        offset: (curPage - 1) * pageSize
-      };
-    };
-
-    pager.setTotal = function (t) {
-      total = t;
-      numPages = Math.ceil(total / pageSize);
-      update();
-    };
 
     return pager;
   };
