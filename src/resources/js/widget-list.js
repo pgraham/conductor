048d50e8e7ce8d3fc975f4f55e3c574548b16249
Philip Graham <philip.robert.graham@gmail.com>
2012-04-17 14:23:10 +0000

    Updated list widget to use updated layout support and some readability updates

--- src/resources/js/widget-list.js
+++ src/resources/js/widget-list.js
@@ -8,56 +8,65 @@
     CDT.widget = {};
   }
 
+  $.layouts.listLayout = function (list) {
+    var toFill = list.find('th.auto-expand'),
+        width = list.width(),
+        allocated = 0,
+        remaining,
+        fill;
+
+    list.find('th').each(function () {
+      if (!$(this).is('.auto-expand')) {
+        allocated += $(this).outerWidth(true);
+      }
+    });
+
+    remaining = width - allocated;
+    fill = Math.floor(remaining / toFill.length);
+    toFill.each(function () {
+      remaining -= fill;
+      $(this).outerWidth(fill, true);
+    });
+    toFill.last().width(toFill.last().width() + remaining);
+
+    list.find('tbody').css('top', list.find('thead').outerHeight(true) + 'px');
+  };
+
+  function buildBasicRenderer(dataIndex) {
+    return function (rowData) {
+      return rowData[dataIndex];
+    }
+  }
+
   /**
    * Create a new list widget
    */
   CDT.widget.list = function () {
-    var list, elm, tbl, thead, headers, selAll, cols, tbody;
+    var list, elm, tbl, thead, tbody, headers, selAll, cols = [];
 
-    cols = [];
-    headers = $('<tr/>');
-    thead = $('<thead/>').append(headers);
-    tbody = $('<tbody/>');
-    tbl = $('<table/>').append(thead).append(tbody);
+    elm = $('<div/>').addClass('cdt-list');
+    tbl = $('<table/>').appendTo(elm);
+    thead = $('<thead/>').addClass('ui-widget-content').appendTo(tbl);
+    tbody = $('<tbody/>').addClass('ui-widget-content').appendTo(tbl);
+    headers = $('<tr/>').appendTo(thead);
 
-    elm = $('<div/>').addClass('cdt-list').append(tbl);
-
-    list = { elm: elm };
-    eventuality(list);
-  
-    selAll = $('<input type="checkbox"/>')
-      .click(function () {
-        tbody.find('.cdt-list-row-selector')
-          .prop('checked', $(this).is(':checked'));
-
-        list.fire('selection-change');
-      });
-    headers.append($('<th/>').addClass('ui-widget-header').append(selAll));
-
-    list.addColumn = function (lbl, renderer, autoExpand) {
-      var dataIndex, elm;
+    function addColumn(lbl, renderer, autoExpand) {
       if (typeof renderer === 'string') {
-        dataIndex = renderer;
-        renderer = function (rowData) {
-          return rowData[dataIndex];
-        };
+        renderer = buildBasicRenderer(renderer);
       }
       cols.push(renderer);
 
-      elm = $('<th/>').addClass('ui-widget-header').append(lbl);
-      headers.append(elm);
+      $('<th/>')
+        .addClass('ui-widget-header')
+        .addClass(autoExpand ? 'auto-expand' : '')
+        .append(lbl)
+        .appendTo(headers);
+    }
 
-      if (autoExpand === true) {
-        elm.addClass('auto-expand');
-      }
-    };
+    function addRow(rowData) {
+      var row = $('<tr/>').appendTo(tbody);
 
-    list.addRow = function (rowData) {
-      var chk, row;
-
-      row = $('<tr/>');
-
-      chk = $('<input type="checkbox"/>')
+      $('<input type="checkbox"/>')
         .addClass('cdt-list-row-selector')
         .data('row-attributes', rowData)
         .click(function () {
@@ -67,9 +76,10 @@
           );
 
           list.fire('selection-change');
-        });
-
-      row.append( $('<td/>').addClass('ui-widget-content').append(chk) );
+        })
+        .appendTo(
+          $('<td/>').addClass('ui-widget-content left-col').appendTo(row)
+        );
 
       $.each(cols, function (idx, col) {
         row.append(
@@ -79,64 +89,62 @@
         );
       });
 
-      tbody.append(row);
-    };
+      row.children().each(function (idx) {
+        var hdrWidth = headers.children().eq(idx).outerWidth(true)
+        $(this).outerWidth(hdrWidth, true);
+      });
+    }
 
-    list.getSelected = function () {
+    function getSelected() {
       return tbody.find('input:checked');
-    };
+    }
 
-    list.populate = function (data, mapper) {
+    function populate(data, mapper) {
       tbody.empty();
       selAll.prop('checked', false);
+
+      if ($.isEmptyObject(data)) {
+        selAll.prop('disabled', true);
+        $('<td/>')
+          .attr('colspan', headers.children().length)
+          .addClass('no-data')
+          .text('No data to display')
+          .appendTo( $('<tr/>').appendTo(tbody) )
+          .outerWidth(tbody.outerWidth())
+          .outerHeight(tbody.outerHeight());
+        return;
+      }
+
+      selAll.prop('disabled', false);
 
       $.each(data, function (idx, rowData) {
         if (mapper !== undefined) {
           rowData = mapper(rowData);
         }
-        list.addRow(rowData);
+        addRow(rowData);
       });
 
       list.fire('load');
+    }
+
+    list = {
+      elm: elm,
+      addColumn: addColumn,
+      getSelected: getSelected,
+      populate: populate
     };
+    eventuality(list);
+  
+    selAll = $('<input type="checkbox"/>').click(function () {
+      tbody.find('.cdt-list-row-selector')
+        .prop('checked', $(this).is(':checked'));
 
-    // Register a layout function that fills the table with the auto expand
-    // column if specified and a layout that fills the list div with the table
-    $(document).ready(function () {
-      var listLayout;
-
-      listLayout = function () {
-        var toFill = headers.find('th.auto-expand'),
-            width = tbl.width(),
-            allocated = 0,
-            remaining,
-            fill;
-
-        headers.children().each(function () {
-          if (!$(this).is('.auto-expand')) {
-            allocated += $(this).outerWidth(true);
-          }
-        });
-
-        remaining = width - allocated;
-        fill = remaining / toFill.length;
-        toFill.each(function () {
-          var $this = $(this), margin, border, padding;
-
-          margin = parseInt($this.css('margin-left'), 10) +
-                   parseInt($this.css('margin-right'), 10);
-          border = parseInt($this.css('border-left-width'), 10) +
-                   parseInt($this.css('border-right-width'), 10);
-          padding = parseInt($this.css('padding-left'), 10) + 
-                    parseInt($this.css('padding-right'), 10);
-
-          $this.width(fill - (margin + border + padding));
-        });
-      };
-      listLayout();
-      CDT.layout.register(listLayout);
-
+      list.fire('selection-change');
     });
+    headers.append($('<th/>').addClass('ui-widget-header left-col').append(selAll));
+
+    // Apply list layout
+    elm.layout('listLayout');
 
     return list;
   };
