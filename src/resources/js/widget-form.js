7470488885bb59f976fa61039fa4e719029b3605
Philip Graham <philip.robert.graham@gmail.com>
2012-04-21 20:32:49 +0000

    JS form widget improvements

--- src/resources/js/widget-form.js
+++ src/resources/js/widget-form.js
@@ -1,79 +1,107 @@
 (function ($, CDT, undefined) {
   "use strict";
 
-  var createLbl, spinner, textArea, textInput;
-  
-  createLbl = function (forInput, lbl) {
+  var defaultSpinnerOpts = { step: 1 },
+      defaultFieldOps = {
+        clearInvalid: function () {
+          this.removeClass('invalid');
+        },
+        getName: function () {
+          return this.attr('name');
+        },
+        getValue: function () {
+          return this.val() === ''
+            ? null
+            : this.val();
+        },
+        markInvalid: function () {
+          this.addClass('invalid');
+        },
+        setValue: function (val) {
+          this.val(val);
+        }
+      };
+
+  function createLbl(forInput, lbl) {
     return $('<label/>')
       .attr('for', forInput)
       .text(lbl)
       .css('float', 'left');
-  };
+  }
 
-  spinner = function (name) {
-    var elm = $('<span/>')
-      .append( $('<input/>').attr('name', name).attr('value', '') );
+  function fieldify(elm, overrides) {
+    return $.extend(elm, defaultFieldOps, overrides || {});
+  }
+
+  CDT.form = {};
+
+  CDT.form.spinner = function (name, opts) {
+    var cont, elm, inpt;
     
-    elm.find('input').spinner({ step: 1 });
+    
+    cont = $('<span/>');
+    inpt = $('<input type="text"/>')
+      .attr('name', name)
+      .attr('value', '')
+      .appendTo(cont)
+      // Order is important, input must be attached before spinner(...) is
+      // called
+      .spinner($.extend({}, defaultSpinnerOpts, opts || {}))
+      .bind('spin', function () {
+        cont.clearInvalid();
+      });
+
+    elm = cont.find('.ui-spinner');
 
     // the latest version of the spinner widget sets an explicit height on the
     // element the same as the calculated height to fix an IE6 bug, but since
     // the spinner isn't attached yet the calculated height is 0.  Since IE6 is
     // not supported by conductor app the height: 0 is simply cleared
-    elm.children().filter('span').css('height', '');
+    elm.css('height', '');
 
-    return {
-      name: name,
-      elm: elm,
+    return fieldify(cont, {
+      clearInvalid: function () {
+        elm.removeClass('invalid');
+      },
+      getName: function () {
+        return name;
+      },
       getValue: function () {
-        return elm.find('input').spinner('value');
+        return inpt.spinner('value');
+      },
+      markInvalid: function () {
+        elm.addClass('invalid');
       },
       setValue: function (val) {
-        elm.find('input').spinner('value', val);
+        inpt.spinner('value', val);
       }
-    };
+    });
   };
 
-  textArea = function (name) {
+  CDT.form.textArea = function (name) {
     var elm = $('<textarea/>')
       .attr('name', name)
       .addClass('text')
       .addClass('ui-widget-content')
-      .addClass('ui-corner-all');
+      .addClass('ui-corner-all')
+      .change(function () {
+        elm.clearInvalid();
+      });
 
-    return {
-      name: name,
-      elm: elm,
-      getValue: function () {
-        return elm.val() === ''
-          ? null
-          : elm.val();
-      },
-      setValue: function (val) {
-        elm.val(val);
-      }
-    };
+    return fieldify(elm);
   };
 
-  textInput = function (name) {
+  CDT.form.textInput = function (name) {
     var elm = $('<input type="text"/>')
       .attr('name', name)
       .addClass('text')
       .addClass('ui-widget-content')
-      .addClass('ui-corner-all');
+      .addClass('ui-corner-all')
+      .change(function () {
+        elm.clearInvalid();
+      });
     
-    return {
-      name: name,
-      elm: elm,
-      getValue: function () {
-        return elm.val() === ''
-          ? null
-          : elm.val();
-      },
-      setValue: function (val) {
-        elm.val(val);
-      }
-    };
+    return fieldify(elm);
   };
 
   if (CDT.widget === undefined) {
@@ -86,30 +114,26 @@
         addInput;
 
     inputs = {};
-    flds = $('<fieldset/>');
-    elm = $('<form/>')
-      .addClass('cdt-form')
-      .append(flds);
+    elm = $('<form/>').addClass('cdt-form');
+    flds = $('<fieldset/>').appendTo(elm);
 
     addInput = function (input, lbl) {
-      inputs[input.name] = input;
-      flds.append(createLbl(input.name, lbl));
-      flds.append(input.elm);
+      inputs[input.getName()] = input;
+      flds.append(createLbl(input.getName(), lbl));
+      flds.append(input);
     };
 
-    return {
-      elm: elm,
+    return $.extend(elm, {
 
       addSpinner: function (name, lbl) {
-        addInput(spinner(name), lbl);
+        addInput(CDT.form.spinner(name), lbl);
       },
       addTextArea: function (name, lbl) {
-        addInput(textArea(name), lbl);
+        addInput(CDT.form.textArea(name), lbl);
       },
       addTextInput: function (name, lbl) {
-        addInput(textInput(name), lbl);
+        addInput(CDT.form.textInput(name), lbl);
       },
-
       getData: function () {
         var data = {};
         $.each(inputs, function (name, input) {
@@ -122,7 +146,7 @@
           input.setValue(data[name]);
         });
       }
-    };
+    });
   };
 
 } (jQuery, CDT));
