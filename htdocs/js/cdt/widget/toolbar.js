fdcb0f0d4166878434ae9cffb3e9470465517ef7
Philip Graham <philip.robert.graham@gmail.com>
2012-12-19 15:18:15 +0000

    Added ability to initialize toolbar widget with a list of buttons, to retrieve a button element and to enable/disable buttons

--- htdocs/js/cdt/widget/toolbar.js
+++ htdocs/js/cdt/widget/toolbar.js
@@ -6,6 +6,11 @@
 
     _create: function () {
       this.element.addClass('cdt-toolbar');
+
+      this._buttons = [];
+      if (this.options.buttons) {
+        this.addButtons(this.options.buttons);
+      }
     },
 
     _setOption: function (key, value) {
@@ -14,13 +19,58 @@
     _destroy: function () {
     },
 
-    addButton: function (lbl, btnOpts, hdlr) {
-      this.element.append($('<button/>').text(lbl).button(btnOpts).click(hdlr));
+    addButton: function (btnCfg) {
+      var btn;
+      if (typeof btnCfg === 'string') {
+        switch (btnCfg) {
+          case '-':
+          this.addSeparator();
+          break;
+
+          default:
+          this.addButton({ label: btnCfg });
+          break;
+        }
+      } else {
+        btn = $('<button/>').button(btnCfg).click(btnCfg.handler);
+        this.element.append(btn);
+        this._buttons[btnCfg.label] = btn;
+      }
+      return this;
+    },
+
+    addButtons: function (btnCfgs) {
+      var self = this;
+      $.each(btnCfgs, function (idx, btnCfg) {
+        self.addButton(btnCfg);
+      });
+      return this;
     },
 
     addSeparator: function () {
       this.element.append($('<span/>').addClass('separator'));
+      return this;
+    },
+
+    disable: function (btnId) {
+      this.setEnabled(btnId, false);
+    },
+
+    enable: function (btnId) {
+      this.setEnabled(btnId, true);
+    },
+
+    getButton: function (btnId) {
+      return this._buttons[btnId];
+    },
+
+    setEnabled: function (btnId, enabled) {
+      this._buttons[btnId].button('option', 'disabled', !enabled);
     }
   });
+
+  exports.toolbar = function (opts) {
+    return $('<div/>').toolbar(opts);
+  };
 
 } (jQuery, CDT, CDT.ns('CDT.widget')));
