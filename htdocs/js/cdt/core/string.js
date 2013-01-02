25d5c9270b1a4b8e418afb511926041cb08e8fb2
Philip Graham <philip.robert.graham@gmail.com>
2013-01-02 04:29:38 +0000

    Added a format function to the String prototype

--- htdocs/js/cdt/core/string.js
+++ htdocs/js/cdt/core/string.js
@@ -4,16 +4,25 @@
 (function (exports, undefined) {
   "use strict";
 
-  exports.trim = function (chars) {
-    return this.replace(/^\s+|\s+$/g, '');
-  }
+  exports.format = function () {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+      return args[number] !== undefined
+        ? args[number]
+        : match;
+    });
+  };
 
   exports.ltrim = function () {
     return this.replace(/^\s+/, '');
-  }
+  };
 
   exports.rtime = function () {
     return this.replace(/\s+$/, '');
-  }
+  };
+
+  exports.trim = function (chars) {
+    return this.replace(/^\s+|\s+$/g, '');
+  };
 
 } (String.prototype));
