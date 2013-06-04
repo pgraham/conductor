c6b61dcc6874d8023fb7f51ec0fb28e40fb6f3ed
Philip Graham <philip.robert.graham@gmail.com>
2013-06-04 14:44:05 +0000

    CS Fixes - Retabbing

--- zpt/cdt/srvc/GlobalConfigService.php
+++ zpt/cdt/srvc/GlobalConfigService.php
@@ -5,7 +5,7 @@
  * All rights reserved.
  *
  * This file is part of Conductor and is licensed by the Copyright holder under
- * the 3-clause BSD License.  The full text of the license can be found in the
+ * the 3-clause BSD License.	The full text of the license can be found in the
  * LICENSE.txt file included in the root directory of this distribution or at
  * the link below.
  * =============================================================================
@@ -34,89 +34,89 @@ use \zpt\pct\ActorFactory;
  * @Uri /config/{name}
  */
 class GlobalConfigService extends BaseRequestHandler
-    implements BeanRequestHandler
+		implements BeanRequestHandler
 {
 
-  /** @Injected */
-  private $authProvider;
+	/** @Injected */
+	private $authProvider;
 
-  /** @Injected */
-  private $persisterFactory;
+	/** @Injected */
+	private $persisterFactory;
 
-  /** @Injected */
-  private $transformerFactory;
+	/** @Injected */
+	private $transformerFactory;
 
-  private $persister;
+	private $persister;
 
-  private $mappings;
+	private $mappings;
 
-  public function get(Request $request, Response $response) {
-    $configName = $request->getParameter('name');
-    if ($configName === null) {
-      // Get all global configuration values
-      // TODO - Inject this
-      $persister = $this->persisterFactory->get('zpt\cdt\model\ConfigValue');
-      $c = new Criteria();
-      $c->addSelect('name')->addSelect('value')
-        ->addEquals('editable', true)
-        ->addSort('name');
-      $globalConfig = $persister->retrieve($c);
+	public function get(Request $request, Response $response) {
+		$configName = $request->getParameter('name');
+		if ($configName === null) {
+			// Get all global configuration values
+			// TODO - Inject this
+			$persister = $this->persisterFactory->get('zpt\cdt\model\ConfigValue');
+			$c = new Criteria();
+			$c->addSelect('name')->addSelect('value')
+				->addEquals('editable', true)
+				->addSort('name');
+			$globalConfig = $persister->retrieve($c);
 
-      $transformer = $this->transformerFactory->get(
-        'zpt\cdt\model\ConfigValue'
-      );
-      $response->setData($transformer->asCollection($globalConfig));
-      return;
-    }
-  }
+			$transformer = $this->transformerFactory->get(
+				'zpt\cdt\model\ConfigValue'
+			);
+			$response->setData($transformer->asCollection($globalConfig));
+			return;
+		}
+	}
 
-  public function put(Request $request, Response $response) {
-    if (!$this->authProvider->hasPermission('cdt-admin')) {
-      throw new RestException(401);
-    }
+	public function put(Request $request, Response $response) {
+		if (!$this->authProvider->hasPermission('cdt-admin')) {
+			throw new RestException(401);
+		}
 
-    $configName = $request->getParameter('name');
-    if ($configName === null) {
-      throw new RestException(405, array( 'Allow: GET' ));
-    }
+		$configName = $request->getParameter('name');
+		if ($configName === null) {
+			throw new RestException(405, array( 'Allow: GET' ));
+		}
 
-    $configValue = $request->getData();
+		$configValue = $request->getData();
 
-    $c = new Criteria();
-    $c->addEquals('name', $configName);
+		$c = new Criteria();
+		$c->addEquals('name', $configName);
 
-    $persister = $this->persisterFactory->get('zpt\cdt\model\ConfigValue');
-    $model = $persister->retrieveOne($c);
-    if ($model === null) {
-      throw new RestException(404);
-    }
+		$persister = $this->persisterFactory->get('zpt\cdt\model\ConfigValue');
+		$model = $persister->retrieveOne($c);
+		if ($model === null) {
+			throw new RestException(404);
+		}
 
-    $model->setValue($configValue);
-    $persister->save($model);
+		$model->setValue($configValue);
+		$persister->save($model);
 
-    $response->setData(array(
-      'success' => true,
-      'msg' => "$configName has been updated"
-    ));
-  }
+		$response->setData(array(
+			'success' => true,
+			'msg' => "$configName has been updated"
+		));
+	}
 
-  public function getMappings() {
-    return $this->mappings;
-  }
+	public function getMappings() {
+		return $this->mappings;
+	}
 
-  public function setAuthProvider($authProvider) {
-    $this->authProvider = $authProvider;
-  }
+	public function setAuthProvider($authProvider) {
+		$this->authProvider = $authProvider;
+	}
 
-  public function setMappings(array $mappings) {
-    $this->mappings = $mappings;
-  }
+	public function setMappings(array $mappings) {
+		$this->mappings = $mappings;
+	}
 
-  public function setPersisterFactory(ActorFactory $persisterFactory) {
-    $this->persisterFactory = $persisterFactory;
-  }
+	public function setPersisterFactory(ActorFactory $persisterFactory) {
+		$this->persisterFactory = $persisterFactory;
+	}
 
-  public function setTransformerFactory(ActorFactory $transformerFactory) {
-    $this->transformerFactory = $transformerFactory;
-  }
+	public function setTransformerFactory(ActorFactory $transformerFactory) {
+		$this->transformerFactory = $transformerFactory;
+	}
 }
