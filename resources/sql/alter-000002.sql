018618fe7dd15e50e8f54179aafa069fcd7920d8
Philip Graham <philip.robert.graham@gmail.com>
2013-12-17 00:26:44 +0000

    Updated alter scripts to use ANSI compliant syntax

--- resources/sql/alter-000002.sql
+++ resources/sql/alter-000002.sql
@@ -9,11 +9,11 @@
 -- one per browser per device.
 --
 
-CREATE TABLE `visitors` (
-  `id`             integer      AUTO_INCREMENT PRIMARY KEY,
-  `key`            varchar(32)  NOT NULL UNIQUE
+CREATE TABLE "visitors" (
+  "id"             integer      AUTO_INCREMENT PRIMARY KEY,
+  "key"            varchar(32)  NOT NULL UNIQUE
 );
 
 -- Insert a configuration value for tracking the conductor alters that have been
 -- applied to the database.
-UPDATE `config_values` SET `value` = '2' WHERE `name` = 'cdt-alter';
+UPDATE "config_values" SET "value" = '2' WHERE "name" = 'cdt-alter';
