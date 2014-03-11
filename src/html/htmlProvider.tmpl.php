adecc93b7b5e3262fa54b8cde5e9afd2a48beeee
Philip Graham <philip.robert.graham@gmail.com>
2014-03-11 16:36:35 +0000

    [minor] License cleanup

--- src/html/htmlProvider.tmpl.php
+++ src/html/htmlProvider.tmpl.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * =============================================================================
+ * Copyright (c) 2014, Philip Graham
+ * All rights reserved.
+ *
+ * This file is part of Conductor and is licensed by the Copyright holder under
+ * the 3-clause BSD License. The full text of the license can be found in the
+ * LICENSE.txt file included in the root directory of this distribution or at
+ * the link below.
+ * =============================================================================
+ *
+ * @license http://www.opensource.org/licenses/bsd-license.php
+ */
 namespace /*# companionNs #*/;
 
 use Psr\Log\LoggerAwareInterface;
