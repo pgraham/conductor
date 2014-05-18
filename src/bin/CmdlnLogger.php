0569c71ddba3df02fedb70f95349a1c75012ec8a
Philip Graham <philip.robert.graham@gmail.com>
2014-05-18 18:37:07 +0000

    [minor] Whitespace

--- src/bin/CmdlnLogger.php
+++ src/bin/CmdlnLogger.php
@@ -3,7 +3,6 @@
  * Copyright (c) 2014, Philip Graham
  * All rights reserved.
  *
- *
  * This file is part of Conductor. For the full copyright and license information
  * please view the LICENSE file that was distributed with this source code.
  */
