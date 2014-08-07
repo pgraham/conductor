27e59c47c5aea467779d6537f551d24675628d59
Philip Graham <philip.robert.graham@gmail.com>
2014-08-07 13:04:13 +0000

    [minor] CS fixes

--- src/ConfigValueProvider.php
+++ src/ConfigValueProvider.php
@@ -1,59 +1,57 @@
 <?php
-/**
- * =============================================================================
- * Copyright (c) 2010, Philip Graham
+/*
+ * Copyright (c) 2014, Philip Graham
  * All rights reserved.
  *
- * This file is part of Conductor and is licensed by the Copyright holder under
- * the 3-clause BSD License.  The full text of the license can be found in the
- * LICENSE.txt file included in the root directory of this distribution or at
- * the link below.
- * =============================================================================
- *
- * @license http://www.opensource.org/licenses/bsd-license.php
+ * This file is part of Conductor. For the full copyright and license information
+ * please view the LICENSE file that was distributed with this source code.
  */
 namespace zpt\cdt;
 
-use \zpt\opal\CompanionLoader;
-use \zpt\orm\Criteria;
+use zpt\opal\CompanionLoader;
+use zpt\orm\Criteria;
 
 /**
  * This class retrieves configuration values from the database.
  *
+ * These values differ from configuration in conductor.yml in that they are
+ * visible to, and possibly modifiable by, the user.
+ *
  * @author Philip Graham <philip@zeptech.ca>
  */
-class ConfigValueProvider {
+class ConfigValueProvider
+{
 
-  private $persister;
+	private $persister;
 
-  public function __construct(CompanionLoader $companionLoader = null) {
-    if ($companionLoader === null) {
-      $companionLoader = new CompanionLoader();
-    }
+	public function __construct(CompanionLoader $companionLoader = null) {
+		if ($companionLoader === null) {
+			$companionLoader = new CompanionLoader();
+		}
 
-    $this->persister = $companionLoader->get(
-      'zpt\dyn\orm\persister',
-      'zpt\cdt\model\ConfigValue'
-    );
-  }
+		$this->persister = $companionLoader->get(
+			'zpt\dyn\orm\persister',
+			'zpt\cdt\model\ConfigValue'
+		);
+	}
 
-  /**
-   * Retrieve the configuration value with the given name.  In order for this to
-   * work the database must be setup to handle configuration values.
-   *
-   * @param string $name The name of the configuration value to retrieve.
-   */
-  public function getConfigValue($name) {
+	/**
+	 * Retrieve the configuration value with the given name.	In order for this to
+	 * work the database must be setup to handle configuration values.
+	 *
+	 * @param string $name The name of the configuration value to retrieve.
+	 */
+	public function getConfigValue($name) {
 
-    $c = new Criteria();
-    $c->addEquals('name', $name);
+		$c = new Criteria();
+		$c->addEquals('name', $name);
 
-    $rows = $this->persister->retrieve($c);
-    if (count($rows) === 0) {
-      return null;
-    }
+		$rows = $this->persister->retrieve($c);
+		if (count($rows) === 0) {
+			return null;
+		}
 
-    $obj = $rows[0];
-    return $obj->getValue();
-  }
+		$obj = $rows[0];
+		return $obj->getValue();
+	}
 }
