92df1798184455099afefe7838ce2b3e14c28b8e
Philip Graham <philip.robert.graham@gmail.com>
2014-08-07 13:59:12 +0000

    [minor] Cs fixes

--- src/compile/DependencyInjectionCompiler.php
+++ src/compile/DependencyInjectionCompiler.php
@@ -1,16 +1,10 @@
 <?php
-/**
- * =============================================================================
- * Copyright (c) 2010, Philip Graham
+/*
+ * Copyright (c) 2014, Philip Graham
  * All rights reserved.
  *
- * This file is part of Conductor and is licensed by the Copyright holder under
- * the 3-clause BSD License.  The full text of the license can be found in the
- * LICENSE.txt file included in the root directory of this distribution or at
- * the link below.
- * =============================================================================
- *
- * @license http://www.opensource.org/licenses/bsd-license.php
+ * This file is part of Conductor. For the full copyright and license information
+ * please view the LICENSE file that was distributed with this source code.
  */
 namespace zpt\cdt\compile;
 
@@ -24,129 +18,133 @@ use zpt\orm\companion\ValidatorGenerator;
 use zpt\pct\TemplateResolver;
 
 /**
- * This class compiles a script which initializes the dependency injection
- * container.  Beans which are inserted into the container are parsed from
- * XML files defined by conductor and my any installed modules.
+ * This class compiles a script which initializes a dependency injection
+ * container.
+ *
+ * Beans are parsed from XML files defined by conductor, any installed modules
+ * and the site. Some beans are also added programatically by other compilers in
+ * the compilation process.
  *
  * @author Philip Graham <philip@zeptech.ca>
  */
-class DependencyInjectionCompiler implements Compiler {
+class DependencyInjectionCompiler implements Compiler
+{
 
-  private $_files = array();
-  private $_beans = array();
+	private $files = array();
+	private $beans = array();
 
-  private $_tmplParser;
+	private $tmplParser;
 
-  public function addBean($id, $class, $props = array()) {
-    $bean = DependencyParser::parse($id, $class);
+	public function addBean($id, $class, $props = array()) {
+		$bean = DependencyParser::parse($id, $class);
 
-    // Merge annotation configured beans with spefied bean property values.
-    // Specified property values override annotation configuration.
-    $bean['props'] = array_merge(
-      $bean['props'],
-      $props
-    );
+		// Merge annotation configured beans with spefied bean property values.
+		// Specified property values override annotation configuration.
+		$bean['props'] = array_merge(
+			$bean['props'],
+			$props
+		);
 
-    $this->_beans[] = $bean;
-  }
+		$this->beans[] = $bean;
+	}
 
-  public function addFile($file) {
-    if (file_exists($file)) {
-      $this->_files[] = $file;
-    }
+	public function addFile($file) {
+		if (file_exists($file)) {
+			$this->files[] = $file;
+		}
 
-    $cfg = simplexml_load_file($file, 'SimpleXMLElement',
-      LIBXML_NOCDATA);
+		$cfg = simplexml_load_file($file, 'SimpleXMLElement',
+			LIBXML_NOCDATA);
 
-    foreach ($cfg->bean as $beanDef) {
-      $bean['id'] = $beanDef['id'];
-      $bean['class'] = $beanDef['class'];
+		foreach ($cfg->bean as $beanDef) {
+			$bean['id'] = $beanDef['id'];
+			$bean['class'] = $beanDef['class'];
 
-      // Parse any annotation configuration or marker interfaces before
-      // applying XML configuration
-      $bean = DependencyParser::parse(
-        (string) $beanDef['id'],
-        (string) $beanDef['class']
-      );
+			// Parse any annotation configuration or marker interfaces before
+			// applying XML configuration
+			$bean = DependencyParser::parse(
+				(string) $beanDef['id'],
+				(string) $beanDef['class']
+			);
 
-      if (isset($beanDef['initMethod'])) {
-        $bean['init'] = (string) $beanDef['initMethod'];
-      }
+			if (isset($beanDef['initMethod'])) {
+				$bean['init'] = (string) $beanDef['initMethod'];
+			}
 
-      $props = array();
-      if (isset($beanDef->property)) {
-        $propDefs = $beanDef->property;
+			$props = array();
+			if (isset($beanDef->property)) {
+				$propDefs = $beanDef->property;
 
-        foreach ($propDefs as $propDef) {
-          $prop = array();
-          $prop['name'] = (string) $propDef['name'];
+				foreach ($propDefs as $propDef) {
+					$prop = array();
+					$prop['name'] = (string) $propDef['name'];
 
-          if (isset($propDef['value'])) {
-            $prop['val'] = $this->getScalar((string) $propDef['value']);
-          } else if (isset($propDef['ref'])) {
-            $prop['ref'] = (string) $propDef['ref'];
-          } else if (isset($propDef['type'])) {
-            $prop['type'] = (string) $propDef['type'];
-          } else {
-            // TODO Warn about an invalid bean definition
-          }
-          $props[] = $prop;
-        }
-      }
-      $bean['props'] = array_merge($bean['props'], $props);
+					if (isset($propDef['value'])) {
+						$prop['val'] = $this->getScalar((string) $propDef['value']);
+					} else if (isset($propDef['ref'])) {
+						$prop['ref'] = (string) $propDef['ref'];
+					} else if (isset($propDef['type'])) {
+						$prop['type'] = (string) $propDef['type'];
+					} else {
+						// TODO Warn about an invalid bean definition
+					}
+					$props[] = $prop;
+				}
+			}
+			$bean['props'] = array_merge($bean['props'], $props);
 
-      $ctorArgs = array();
-      if (isset($beanDef->ctorArg)) {
-        $ctor = $beanDef->ctorArg;
+			$ctorArgs = array();
+			if (isset($beanDef->ctorArg)) {
+				$ctor = $beanDef->ctorArg;
 
-        // TODO Is order guaranteed by XML parser?
-        // TODO Combine this with logic in dependency parser to eliminate
-        //      duplication
-        foreach ($ctor as $arg) {
-          if (isset($arg['value'])) {
-            $ctorArgs[] = $this->getScalar((string) $arg['value'], true);
-          } else if (isset($arg['ref'])) {
-            $ctorArgs[] = '$' . ((string) $arg['ref']);
-          } else {
-            // TODO Warn about an invalid bean definition
-          }
-        }
-      }
-      $bean['ctor'] = $ctorArgs;
+				// TODO Is order guaranteed by XML parser?
+				// TODO Combine this with logic in dependency parser to eliminate
+				//			duplication
+				foreach ($ctor as $arg) {
+					if (isset($arg['value'])) {
+						$ctorArgs[] = $this->getScalar((string) $arg['value'], true);
+					} else if (isset($arg['ref'])) {
+						$ctorArgs[] = '$' . ((string) $arg['ref']);
+					} else {
+						// TODO Warn about an invalid bean definition
+					}
+				}
+			}
+			$bean['ctor'] = $ctorArgs;
 
-      $this->_beans[] = $bean;
-    }
-  }
+			$this->beans[] = $bean;
+		}
+	}
 
-  public function compile(RuntimeConfig $config) {
-    $dynTarget = $config->getDynamicClassTarget();
+	public function compile(RuntimeConfig $config) {
+		$dynTarget = $config->getDynamicClassTarget();
 
-    // Build the InjectionConfiguration script
-    $srcPath = __DIR__ . '/InjectionConfigurator.tmpl.php';
-    $outPath = $dynTarget->getPath()->pathJoin('InjectionConfigurator.php');
-    $values = array(
-      'namespace' => $dynTarget->getPrefix()->rtrim('\\')->__toString(),
-      'beans' => $this->_beans
-    );
-    $tmplResolver = new TemplateResolver($this->_tmplParser);
-    $tmplResolver->resolve($srcPath, $outPath, $values);
-  }
+		// Build the InjectionConfiguration script
+		$srcPath = __DIR__ . '/InjectionConfigurator.tmpl.php';
+		$outPath = $dynTarget->getPath()->pathJoin('InjectionConfigurator.php');
+		$values = array(
+			'namespace' => $dynTarget->getPrefix()->rtrim('\\')->__toString(),
+			'beans' => $this->beans
+		);
+		$tmplResolver = new TemplateResolver($this->tmplParser);
+		$tmplResolver->resolve($srcPath, $outPath, $values);
+	}
 
-  public function setTemplateParser($templateParser) {
-    $this->_tmplParser = $templateParser;
-  }
+	public function setTemplateParser($templateParser) {
+		$this->tmplParser = $templateParser;
+	}
 
-  private function getScalar($val, $quoteStrings = false)
-  {
-      if (is_numeric($val)) {
-          return (float) $val;
-      } else if (strtolower($val) === 'true') {
-          return true;
-      } else if (strtolower($val) === 'false') {
-          return false;
-      } else if (strtolower($val) === 'null') {
-          return null;
-      }
-      return $quoteStrings ? "'" . $val . "'" : $val;
-  }
+	private function getScalar($val, $quoteStrings = false)
+	{
+			if (is_numeric($val)) {
+					return (float) $val;
+			} else if (strtolower($val) === 'true') {
+					return true;
+			} else if (strtolower($val) === 'false') {
+					return false;
+			} else if (strtolower($val) === 'null') {
+					return null;
+			}
+			return $quoteStrings ? "'" . $val . "'" : $val;
+	}
 }
